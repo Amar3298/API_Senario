4acadac58b67af67e21764cda5f865c4c6131ee7
Amar <rautamar705@gmail.com>
2023-10-23 14:48:24 +0530

    Right Outer Join

--- Join/Right Outer Join/right.sql
+++ Join/Right Outer Join/right.sql
@@ -1,1 +1,7 @@
-SELECT * from
+-- The Address Table has 78 Rows 
+-- The Bussiness Partner Table has 45 rows
+Select * from "FL_HDI_DB_1"."MD.Addresses" as A
+RIGHT OUTER JOIN "FL_HDI_DB_1"."MD.BusinessPartner" as B
+on A."ADDRESSID" = B."ADDRESSES.ADDRESSID";
+
+In this case right table is BusinessPartner it will take entire data from the right table so the output will have 45 rows
