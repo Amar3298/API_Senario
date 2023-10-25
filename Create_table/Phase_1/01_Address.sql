079e5b3acb0710bede75ca0c8b82845b9cbab803
Amar <rautamar705@gmail.com>
2023-10-25 18:19:58 +0530

    No one is your they just pretend lets move back to plane

---
+++ Create_table/Phase_1/01_Address.sql
@@ -0,0 +1,14 @@
+CREATE COLUMN TABLE "Addresses"  (
+    "ADDRESSID" INTEGER NOT NULL COMMENT 'Address ID',
+    "CITY" VARCHAR(40) COMMENT 'City',
+    "POSTALCODE" NVARCHAR(10) COMMENT 'Postal Code',
+    "STREET" NVARCHAR(60) COMMENT 'Street',
+    "BUILDING" NVARCHAR(10) COMMENT 'Building Number',
+    "COUNTRY" NVARCHAR(3) COMMENT 'Country',
+    "REGION" NVARCHAR(4) COMMENT 'Region Otherwise Known as State in some countries',
+    "ADDRESSTYPE" NVARCHAR(2) COMMENT 'Address Type',
+    "VALIDITY.STARTDATE" DATE  COMMENT 'Start Date',
+    "VALIDITY.ENDDATE" DATE  COMMENT 'End Date',
+    "LATITUDE" DOUBLE  COMMENT 'Geo Latitude',
+    "LONGITUDE" DOUBLE  COMMENT 'Geo Longitude',
+    PRIMARY KEY ("ADDRESSID"));	
